a8a90b3fb84bd82ec76aa4896c6be2e9cf39baa0
Jonathan Tay <jontay999@gmail.com>
2024-08-13 11:53:20 +0800

    finished random1

--- Crypto/random1/distrib/random1.py
+++ Crypto/random1/distrib/random1.py
@@ -1,3 +1,17 @@
-from random import randint
 
+from random import getrandbits
 
+flag = "REP{mersenne_twister_is_bad_for_encryption}"
+output = []
+# Here's some random output
+for _ in range(620):
+    output.append(getrandbits(32))
+
+# Here's the flag, try to break the encryption :)
+for i in range(len(flag)):
+    random_byte = getrandbits(32)
+    output.append(ord(flag[i]) ^ random_byte)
+
+with open("random.txt", "w") as f:
+    output = "\n".join(map(str, output))
+    f.write(output)
