f5281886d0e9a6389dbd3bba671bd7e571301274
Jonathan Tay <jontay999@gmail.com>
2024-08-15 20:58:30 +0800

    basic frontend

---
+++ Web/sqli1/distrib/client/index.js
@@ -0,0 +1,43 @@
+function fancy_schmancy_effect() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
+    document.querySelector("h1").onmouseover = event => {
+        let iteration = 0;
+        clearInterval(interval);
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return event.target.dataset.value[index];
+                    }
+
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+
+            if (iteration >= event.target.dataset.value.length) {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+        }, 30);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fancy_schmancy_effect()
+
+
+
+    document.getElementById("form").addEventListener('submit', e => {
+        e.preventDefault()
+        const { username, password } = e.target
+        const data = {
+            username: username.value,
+            password: password.value
+        }
+        alert(username.value + password.value)
+    })
+
+});
