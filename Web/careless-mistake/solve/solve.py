8660a2b72b0ac5392512100d9b5faa1cec253139
Jonathan Tay <jontay999@gmail.com>
2024-08-19 15:20:57 +0800

    finish deployments

--- Web/careless-mistake/solve/solve.py
+++ Web/careless-mistake/solve/solve.py
@@ -4,7 +4,7 @@ import hashlib
 import uuid
 
 
-address = "http://127.0.0.1:3000/note"
+address = "http://localhost:3002/note"
 secret = f"secret-key-{uuid.uuid4}"[:32]
 note_id = 0
 token = hmac.new(
