9b8ccf8c24710cc553f19eb08da87c3138495d94
Jonathan Tay <jontay999@gmail.com>
2024-08-19 22:13:21 +0800

    almost done

--- Web/xss1/service/public/index.js
+++ Web/xss1/service/public/index.js
@@ -1,13 +1,27 @@
+const question_keys = ["math", "chemical_eng", "eee"]
+const num_qns = question_keys.length
+const open_ended = "materials_eng"
+
+
+// If the query parameters are set, fill in the form.
+function read_query_params() {
+    valid_values = ["true", "false"]
+    const params = new URLSearchParams(window.location.search)
+    params.forEach((value, key) => {
+        if (key === open_ended) {
+            document.getElementById(open_ended).value = value
+        } else if (question_keys.includes(key) && valid_values.includes(value)) {
+            document.querySelector(`input[name="${key}"][value="${value}"]`).checked = true;
+        };
+    });
+}
+
 const submit_form = () => {
     const answers = {}
-    for (let i = 0; i < 3; i++) {
-        answers[i] = document.querySelector(`input[name="${i}"]:checked`)?.value;
+    for (let qn_key of question_keys) {
+        answers[qn_key] = document.querySelector(`input[name="${qn_key}"]:checked`)?.value;
     }
-    answers[3] = document.querySelector('textarea').value
-    const query_string = new URLSearchParams(answers).toString();
-    console.log(query_string)
-    alert(query_string)
-
+    answers[open_ended] = document.querySelector('textarea').value
     fetch('/submit', {
         method: 'POST',
         headers: {
@@ -16,11 +30,12 @@ const submit_form = () => {
         body: JSON.stringify(answers)
     })
         .then(response => response.json())
-        .then(data => console.log('Success:', data))
+        .then(data => alert(JSON.stringify(data)))
         .catch(error => console.error('Error:', error));
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    read_query_params()
     document.getElementById("emc-submit").addEventListener('click', submit_form)
 });
--- Web/xss1/service/server.js
+++ Web/xss1/service/server.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer")
 const fastify = require('fastify')()
 const path = require('path');
 
@@ -6,26 +7,101 @@ fastify.register(require('@fastify/static'), {
     prefix: '/',
 });
 
+// This variable will be initialised later
+// I dont want to keep launching new puppeteer instances so everyone will share this instance of the browser
+let browser;
+const port = 3000;
 
-fastify.post('/submit', async (request, reply) => {
-    try {
-        const answers = request.body;
-        console.log('Received answers:', answers);
-        reply.send({ status: 'success', data: answers });
-    } catch (error) {
-        console.error('Error:', error);
-        reply.status(500).send({ status: 'error', message: 'Internal Server Error' });
+// Since each request to /submit launches my admin bot
+// I limit requests to max 3 per minute on the /submit route
+(async () => {
+    await fastify.register(require('@fastify/rate-limit'), {
+        max: 3,
+        timeWindow: '1 minute',
+        allowList: [],
+        onExceeding: function (request, key) {
+            console.log('callback on exceededing ... executed before response to client')
+        },
+        onExceeded: function (request, key) {
+            console.log('callback on exceeded ... ')
+        }
+    })
+
+    // Well technically I could verify them here but where's the fun in that
+    // let me launch a bot that can visit my own website to see if the answers are posted nicely there
+    // before all that, let me hide my cookie inside
+    const cookies = [{
+        'name': 'flag',
+        'value': 'REP{fake_flag}'
+    }];
+    const verify_answers = async (answers) => {
+        const query_param = new URLSearchParams(answers).toString();
+        const base_url = `http://localhost:${port}`
+        console.log("visiting url:", url)
+        const ctx = await browser.createBrowserContext()
+        const page = await ctx.newPage()
+        await page.goto(base_url, { timeout: 5000, waitUntil: 'networkidle2' })
+        await page.setCookie(...cookies);
+        try {
+            const url_with_answers = `http://localhost:${port}?${query_param}`
+            await page.goto(url_with_answers, { timeout: 5000, waitUntil: 'networkidle2' })
+            await page.waitForSelector('textarea')
+        } finally {
+            await page.close()
+            await ctx.close()
+        }
     }
-});
 
-fastify.get('/', (req, reply) => {
-    reply.sendFile('index.html');
-});
+    fastify.post('/submit', (request, reply) => {
+        console.log("just happily submitting")
+        try {
+            const answers = request.body;
+            // no need to await because I will run this job asynchronously
+            // verify_answers(answers)
+            reply.send({ message: "Answers submitted! I'll get an admin to check those answers soon..." });
+            console.log("can stuff still go on here?")
+            verify_answers(answers)
+        } catch (error) {
+            console.error('Error:', error);
+            reply.status(500).send({ status: 'error', message: 'Internal Server Error' });
+        }
+    });
 
-// Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-})
+    fastify.get('/', {
+        config: {
+            rateLimit: false
+        }
+    }, (req, reply) => {
+        reply.sendFile('index.html');
+    });
+
+    // Run the server!
+    fastify.listen({ port }, async (err, address) => {
+
+        console.log(`[*] Listening on port ${port} ${address}`)
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        }
+
+        browser = await puppeteer.launch({
+            pipe: true,
+            dumpio: true,
+            args: [
+                '--incognito',
+                '--disable-dev-shm-usage', // Docker stuff
+                '--js-flags=--noexpose_wasm,--jitless', // No Chrome n-days please
+                '--disable-background-networking',
+                '--disable-default-apps',
+                '--disable-extensions',
+                '--disable-sync',
+                '--no-first-run' // Skip first-run setup
+            ]
+        })
+
+    })
+
+})();
+// I am sad about this IIFE nonsense due to fastify4 affecting fastify.register(...) and CommonJS not supporting top level awaits
+// if the above sentence means nothing to you, don't worry about it
+// it just explains why the whole server code is wrapped in a bunch of weird brackets
