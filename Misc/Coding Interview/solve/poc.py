c553a918139b0e1198c90ddadb3c348c35216fcf
Jonathan Tay <jontay999@gmail.com>
2024-08-22 11:52:22 +0800

    its kinda done but its a bit unstable

--- Misc/Coding Interview/solve/poc.py
+++ Misc/Coding Interview/solve/poc.py
@@ -1,17 +1,15 @@
-from pwn import *
-
-p = remote("localhost", 4001)
 lines = []
 row = []
 prime = 10273 
 # 20753 also not bad
-for i in range(1,10000-50):
+for i in range(1,10000-40):
     row.append(str(i*10273))
     if i % 10 == 0:
         lines.append('1')
         lines.append(' '.join(row))
         row.clear()
-# lines.append('2')
-for str_line in lines:
-    p.sendline(str_line.encode('utf-8'))
-p.interactive()
+        
+lines.append('2')
+print(len(lines))
+with open('input.txt', 'w') as f:
+    f.write('\n'.join(lines))
