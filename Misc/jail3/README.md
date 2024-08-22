f3180958c066e027b8b52f1cd7b3e892a9ba07b0
Jonathan Tay <jontay999@gmail.com>
2024-08-22 16:42:15 +0800

    tryin to make jail3 to work

---
+++ Misc/jail3/README.md
@@ -0,0 +1,4 @@
+```
+docker build -t repctf-misc-3 .
+docker run -d -p 4003:5000 --name misc-3 --privileged repctf-misc-3
+```
---
+++ Misc/jail3/solve/test.py
@@ -0,0 +1,19 @@
+import unicodedata
+
+# b = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+# d = {}
+# for i in range(0x110000):
+#     c = chr(i)
+#     x = unicodedata.normalize('NFKC', c)
+#     if x != c and x in b:
+#         d[x] = c 
+
+# def form_word(word):
+#     w = ""
+#     for c in word:
+#         w += d[c]
+#     print(w)
+# form_word("print")
+
+# 𝚙𝚛𝚒𝚗𝚝
+eval(input())
